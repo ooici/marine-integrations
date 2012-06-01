2fa36dfdcd7e71b775d8dd0031a80f4d61f50e72
Bill French <wfrench@ucsd.edu>
2012-06-01 08:18:43 -0700

    moved IDK from coi-services to mi

---
+++ mi/idk/idk_rebase.py
@@ -0,0 +1,25 @@
+"""
+@file coi-services/mi.idk/driver_generator.py
+@author Bill French
+@brief Main script class for running the reinitializing 
+       the IDK environment 
+"""
+
+from mi.idk.config import Config
+from pyon.util.log import log
+
+class IDKRebase():
+    """
+    Main class for running the rebase process.
+    """
+
+    def run(self):
+        """
+        @brief Run it.
+        """
+        log.info( "*** IDK Rebase Process***" )
+        Config().rebase()
+
+if __name__ == '__main__':
+    app = IDKRebase()
+    app.run()
