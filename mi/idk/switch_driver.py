e59be91f18b0076c0446a02c878373b25394b0ce
Bill French <wfrench@ucsd.edu>
2012-06-16 22:42:46 -0700

    added new option fo list drivers

--- mi/idk/switch_driver.py
+++ mi/idk/switch_driver.py
@@ -4,10 +4,12 @@
 @brief Main script class for running the switch_driver process
 """
 
-from os.path import exists
+from os.path import exists, join, isdir
+from os import listdir
 
 from mi.idk.metadata import Metadata
 from mi.idk.comm_config import CommConfig
+from mi.idk.config import Config
 from mi.idk.exceptions import DriverDoesNotExist
 
 from mi.core.log import log
@@ -19,14 +21,21 @@ class SwitchDriver():
     Main class for running the switch driver process.
     """
 
+    def __init__(self, make=None, model=None, name=None, version=None):
+        self.driver_make = make
+        self.driver_model = model
+        self.driver_name = name
+        self.driver_version = version
+
     def fetch_metadata(self):
         """
         @brief collect metadata from the user
         """
-        self.driver_make = prompt.text( 'Driver Make' )
-        self.driver_model = prompt.text( 'Driver Model' )
-        self.driver_name = prompt.text( 'Driver Name' )
-        
+        if not (self.driver_make and self.driver_model and self.driver_name):
+            self.driver_make = prompt.text( 'Driver Make', self.driver_make )
+            self.driver_model = prompt.text( 'Driver Model', self.driver_model )
+            self.driver_name = prompt.text( 'Driver Name', self.driver_name )
+
         self.metadata = Metadata(self.driver_make, self.driver_model, self.driver_name)
 
     def fetch_comm_config(self):
@@ -52,7 +61,44 @@ class SwitchDriver():
         self.fetch_comm_config()
         self.metadata.link_current_metadata()
 
+    @staticmethod
+    def list_drivers():
+        driver_dir = join(Config().get("working_repo"), 'mi', 'instrument')
+        log.debug("Driver Dir: %s", driver_dir)
 
-if __name__ == '__main__':
-    app = SwitchDriver()
-    app.run()
+        drivers = SwitchDriver.get_drivers()
+
+        for make in sorted(drivers.keys()):
+            for model in sorted(drivers[make].keys()):
+                for name in sorted(drivers[make][model].keys()):
+                    for version in sorted(drivers[make][model][name]):
+                        print "%s %s %s %s" % (make, model, name, version)
+
+
+    @staticmethod
+    def get_drivers():
+        driver_dir = join(Config().get("working_repo"), 'mi', 'instrument')
+        log.debug("Driver Dir: %s", driver_dir)
+
+        drivers = {}
+
+        for make in listdir(driver_dir):
+            make_dir = join(driver_dir, make)
+            if isdir(make_dir) and not make == 'test':
+                for model in listdir(make_dir):
+                    model_dir = join(make_dir, model)
+                    if isdir(model_dir) and not model == 'test':
+                        for name in listdir(model_dir):
+                            name_dir = join(model_dir, name)
+                            if isdir(name_dir) and not name == 'test':
+                                log.debug("found driver: %s %s %s", make, model, name)
+                                if not drivers.get(make): drivers[make] = {}
+                                if not drivers[make].get(model): drivers[make][model] = {}
+                                drivers[make][model][name] = SwitchDriver.get_versions(make,model,name)
+
+        return drivers
+
+    @staticmethod
+    def get_versions(make, model, name):
+        return ['master']
+
