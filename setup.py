88699b2595269a5a5c72b019b39461b6999b7460
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2012-09-26 11:30:48 -0700

    bump pyon, coi versions; make utilities unversioned

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(  name = 'marine-integrations',
         dependency_links = [
             'http://sddevrepo.oceanobservatories.org/releases/',
             'https://github.com/ooici/pyon/tarball/master#egg=pyon',
-            'https://github.com/ooici/utilities/tarball/v2012.9.13.15#egg=utilities-2012.9.13.15'
+            'https://github.com/ooici/utilities/tarball/master#egg=utilities-9999'
         ],
         test_suite = 'pyon',
         entry_points = {
@@ -41,6 +41,6 @@ setup(  name = 'marine-integrations',
             'gitpy==0.6.0',
             'snakefood==1.4',
             'ntplib>=0.1.9',
-            'utilities==2012.9.13.15',
+            'utilities',
         ],
      )
