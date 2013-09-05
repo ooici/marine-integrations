603f957f69845d24cd27ee6fe156e523eb64b8e8
Emily Hahn <ehahn@bbn.com>
2013-09-05 13:39:14 -0700

    Created single file harvester for checking if a single file has had data appended to it.  Also updated the test driver template.

--- mi/dataset/harvester.py
+++ mi/dataset/harvester.py
@@ -12,8 +12,10 @@ via data set agents.
 __author__ = 'Christopher Mueller, Jonathan Newbrough, Steve Foley'
 __license__ = 'Apache 2.0'
 
+import os
+
 from mi.core.log import get_logger ; log = get_logger()
-from ooi.poller import DirectoryPoller
+from ooi.poller import DirectoryPoller, ConditionPoller
 
 class Harvester(object):
     """ abstract class to show API needed for plugin poller objects """
@@ -37,6 +39,9 @@ class Harvester(object):
 ## added here down the road
 
 class AdditiveSequentialFileHarvester(DirectoryPoller, Harvester):
+    """
+    Poll a single directory looking for new files with the directory poller.
+    """
     def __init__(self, config, memento, file_callback, exception_callback):
         if not isinstance(config, dict):
             raise TypeError("Config object must be a dict")
@@ -51,7 +56,90 @@ class AdditiveSequentialFileHarvester(DirectoryPoller, Harvester):
                                  config.get('frequency', 1))
 
     def on_new_files(self, files):
+        """
+        New files have been found, open each file and process it in the callback
+        """
         for file in files:
             if file>self.last_file_completed:
                 with open(file,'rb') as f:
                     self.callback(f, file)
+                    
+            
+class FilePoller(ConditionPoller):
+    """
+    poll a single file to determine if that file has had additional data appended to it
+    """
+    
+    def __init__(self, fullfile, last_read_offset, callback, exception_callback=None, interval=1):
+        """
+        @param fullfile full file path to the file to monitor
+        @param last_read_offset offset of the last byte read in this file (can be None)
+        @param callback the callback to call when new data has been found in the file
+        @param exception_callback the callback to call when an exception has occurred
+        @param interval the interval between checking on the file in seconds
+        """
+        try:
+            if not os.path.isfile(fullfile):
+                raise ValueError('%s is not an existing file'%fullfile)
+            self._file = fullfile
+            self._last_offset = last_read_offset
+            super(FilePoller,self).__init__(self._check_for_data, callback, exception_callback, interval)
+        except:
+            log.error('failed init?', exc_info=True)
+            
+    def _check_for_data(self):
+        """
+        find out how the last file offset relates to the current file size.  If it is less than the current file size, return the 
+        """
+        filesize = os.path.getsize(self._file)
+        # files, but no change since last time
+        log.debug("Checking file size, size is %d, last offset is %s", filesize, str(self._last_offset))
+        if filesize == 0:
+            # file is empty
+            return None
+        if self._last_offset and filesize and filesize==self._last_offset:
+            # no change since last filesize
+            return None
+        self._last_offset = filesize
+        return self._file
+    
+    
+class SingleFileHarvester(FilePoller, Harvester):
+    """
+    Poll a single file to determine if data has been appended to the file
+    """
+    def __init__(self, config, last_read_offset, data_callback, exception_callback):
+        """
+        @param config a configuration dictionary containing harvester config
+        @param last_read_offset offset of the last byte read in this file (can be None)
+        @param data_callback the callback to call when new data has been found in the file
+        @param exception_callback the callback to call when an exception has occurred
+        """
+        if not isinstance(config, dict):
+            raise TypeError("Config object must be a dict")
+        
+        self.fullfile = config['directory'] + '/' + config['filename']
+        self.callback = data_callback
+        self.last_offset = last_read_offset
+        
+        FilePoller.__init__(self, self.fullfile,
+                            self.last_offset,
+                            self.on_new_data,
+                            exception_callback,
+                            config.get('frequency', 1))
+        
+    def on_new_data(self, fullfile):
+        """
+        When new data has been found, open the file and seek to the last
+        offset if there is one
+        """
+        if fullfile:
+            filesize = os.path.getsize(fullfile)
+            with open(fullfile, 'rb') as f:
+                # If there is a last offset, seek to the last offset so we are
+                # starting where we left off last time in the file
+                if self.last_offset:
+                    log.debug("Seeking to last offset %d", self.last_offset)
+                    f.seek(self.last_offset)
+                self.callback(f, filesize)
+            self.last_offset = filesize
---
+++ mi/dataset/test/test_single_file_harvester.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+
+"""
+@package mi.dataset.test.test_single_file_harvester.py
+@file mi/dataset/test/test_single_file_harvester.py
+@author Emily Hahn
+@brief Test code to exercize single file harvester
+"""
+
+import os
+import gevent
+import time
+
+from mi.core.log import get_logger ; log = get_logger()
+from nose.plugins.attrib import attr
+from mi.core.unit_test import MiUnitTest
+from mi.dataset.harvester import SingleFileHarvester
+
+#bin/nosetests -x -v mi.dataset.test.test_single_file_harvester
+TESTDIR = '/tmp/dsatest'
+FILENAME = 'testfile.txt'
+CONFIG = {'directory': TESTDIR,
+          'filename': FILENAME,
+          'frequency': 2}
+
+
+@attr('INT', group='eoi')
+class TestExternalDatasetHarvester(MiUnitTest):
+    data_found = False
+    
+    def setUp(self):
+        """
+        reset counters and ensure we have test files in place
+        """
+        log.info('*** Starting test %s ***', self._testMethodName)
+        self.data_found = False
+
+        if(not os.path.exists(TESTDIR)):
+            os.makedirs(TESTDIR)
+
+        # Create a file for testing
+        open(os.path.join(TESTDIR, FILENAME), 'a').close()
+
+    def tearDown(self):
+        """
+        Cleanup files we have created
+        """
+        if os.path.isfile(os.path.join(TESTDIR, FILENAME)):
+            os.remove(os.path.join(TESTDIR, FILENAME))
+        
+    def test_harvester_from_scratch(self):
+        """
+        Test that the harvester can find files as they are added to a directory,
+        starting with just the base file in the directory
+        """
+        # start the harvester from scratch
+        file_offset = None
+        file_harvester = SingleFileHarvester(CONFIG, file_offset,
+                                             self.new_data_found_callback,
+                                             self.harvester_exception_callback)
+        file_harvester.start()
+        
+        # start a new event which will write new lines of data into the file
+        self.file_filler = gevent.spawn(self.fill_file_with_data,
+                                             CONFIG['directory'],
+                                             CONFIG['filename'], 2)
+        
+        # Wait for three lines of new data to be discovered
+        self.wait_for_data()
+        self.wait_for_data()
+         
+        file_harvester.shutdown()
+        
+    def test_harvester_with_initial_data(self):
+        """
+        Test that the harvester can find files as they are added to a directory,
+        starting with just the base file in the directory
+        """
+        # start with 2 lines in the file
+        self.fill_file_with_data(CONFIG['directory'], CONFIG['filename'], 2, 0)
+        
+        # start the harvester with data in the file
+        file_offset = None
+        file_harvester = SingleFileHarvester(CONFIG, file_offset,
+                                             self.new_data_found_callback,
+                                             self.harvester_exception_callback)
+        file_harvester.start()
+        
+        # start a new event which will write new lines of data into the file
+        self.file_filler = gevent.spawn(self.fill_file_with_data,
+                                             CONFIG['directory'],
+                                             CONFIG['filename'], 1)
+        
+        # Wait for three lines of new data to be discovered
+        self.wait_for_data()
+        self.wait_for_data()
+         
+        file_harvester.shutdown()
+        
+    def test_harvester_with_offset(self):
+        """
+        Test that the harvester can find files as they are added to a directory,
+        starting with just the base file in the directory
+        """
+        # start with 2 lines in the file
+        self.fill_file_with_data(CONFIG['directory'], CONFIG['filename'], 2, 0)
+        
+        # start the harvester at the end of the current data
+        file_offset = 32
+        file_harvester = SingleFileHarvester(CONFIG, file_offset,
+                                             self.new_data_found_callback,
+                                             self.harvester_exception_callback)
+        file_harvester.start()
+        
+        # start a new event which will write new lines of data into the file
+        self.file_filler = gevent.spawn(self.fill_file_with_data,
+                                             CONFIG['directory'],
+                                             CONFIG['filename'], 2)
+        
+        # Wait for lines of new data to be discovered
+        self.wait_for_data()
+        self.wait_for_data()
+         
+        file_harvester.shutdown()
+        
+    def test_harvester_multi_line(self):
+        """
+        Test that the harvester can find files as they are added to a directory,
+        starting with just the base file in the directory
+        """
+        # start the harvester from scratch
+        file_offset = None
+        file_harvester = SingleFileHarvester(CONFIG, file_offset,
+                                             self.new_data_found_callback,
+                                             self.harvester_exception_callback)
+        file_harvester.start()
+        
+        # set the filler to write 1 line just under every second, meaning that 2 lines will
+        # appear for each wait_for_data
+        self.file_filler = gevent.spawn(self.fill_file_with_data,
+                                             CONFIG['directory'],
+                                             CONFIG['filename'], 4, .9)
+        
+        # Wait for lines of new data to be discovered
+        self.wait_for_data()
+        self.wait_for_data()
+         
+        file_harvester.shutdown()
+        
+    def test_harvester_exception(self):
+        """
+        Verify exceptions
+        """
+        # bad config
+        config = "blah"
+        self.assertRaises(TypeError, SingleFileHarvester, (config, None,
+                                                           self.new_data_found_callback,
+                                                           self.harvester_exception_callback))        
+        
+    def new_data_found_callback(self, file_handle, file_size):
+        """
+        Callback when a new file is found by the harvester.  This should pass the file
+        to the parser, but from this test we don't have the parser, so just close the file. 
+        """
+        current_idx = file_handle.tell()
+        bytes_to_read = file_size - current_idx
+        data = file_handle.read(bytes_to_read)
+        file_handle.close()
+        self.data_found = True
+        log.info("Read new data from %d to %d: %s", current_idx, file_size, data)
+    
+    def harvester_exception_callback(self, exception):
+        """
+        Callback if there is an exception in the harvester, just raise an exception
+        """
+        log.error("Error polling for files: %s", exception)
+        raise Exception("Error polling for files: %s", exception)
+    
+    def fill_file_with_data(self, data_directory, filename, num_lines=1, delay=6):
+        """
+        Copy the first index file to generate files with sequential increasing indices.
+        Search the directory to find the current highest index, then increase one index. 
+        """
+        
+        fullfile = os.path.join(data_directory, filename)
+        if not os.path.isfile(fullfile):
+            # just touch the file to create it
+            open(fullfile, 'a').close()
+        
+        for i in range(0, num_lines):
+            time.sleep(delay)
+            file_handle = open(fullfile, 'a')
+            data = 'new data line %d\n' % i
+            file_handle.write(data)
+            file_handle.close()
+            log.debug("Added file line %s", data)
+            
+    def wait_for_data(self, delay=2, timeout=60):
+        """
+        Wait for a new round of files to be discovered
+        """
+        end_time = 0
+        while(not self.data_found):
+            log.debug("Waiting for next data...")
+            time.sleep(delay)
+            end_time += delay
+            if end_time > timeout:
+                raise Exception("Timeout waiting to find data")
+        self.data_found = False
+
+
+
