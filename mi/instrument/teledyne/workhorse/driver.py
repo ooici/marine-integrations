a51b8242df8d3a90d4259b701ede8d17c815eb62
sahn <locateSung@gmail.com>
2014-04-15 10:44:51 -0700

    Move Parameters

--- mi/instrument/teledyne/workhorse/driver.py
+++ mi/instrument/teledyne/workhorse/driver.py
@@ -28,24 +28,7 @@ class WorkhorseParameter(TeledyneParameter):
     """
     Device parameters
     """
-    #
-    # Workhorse parameters
-    #
-    SERIAL_FLOW_CONTROL = 'CF'
-    BANNER = 'CH'
-    SLEEP_ENABLE = 'CL'
-    SAVE_NVRAM_TO_RECORDER = 'CN'
-    POLLED_MODE = 'CP'
-    PITCH = 'EP'
-    ROLL = 'ER'
 
-    LATENCY_TRIGGER = 'CX'
-    HEADING_ALIGNMENT = 'EA'
-    DATA_STREAM_SELECTION ='PD'
-    ENSEMBLE_PER_BURST ='TC'
-    BUFFERED_OUTPUT_PERIOD ='TX'
-    SAMPLE_AMBIENT_SOUND ='WQ'
-    TRANSDUCER_DEPTH ='ED'
 
 class WorkhorseInstrumentDriver(TeledyneInstrumentDriver):
     """
