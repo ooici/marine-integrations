ce7d996d9a6c7c4dd3b1115c5f33a9aa7df32df3
Steven Myerson <Steven_G_Myerson@raytheon.com>
2014-04-24 13:23:50 -0700

    Initial control after start_driver

---
+++ mi/dataset/parser/vel3d_l_wfp_sio_mule.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+"""
+@package mi.dataset.parser.vel3d_l_wfp
+@file marine-integrations/mi/dataset/parser/vel3d_l_wfp.py
+@author Steve Myerson (Raytheon)
+@brief Parser for the vel3d_l_wfp dataset driver
+Release notes:
+
+Initial Release
+"""
+
+__author__ = 'Steve Myerson (Raytheon)'
+__license__ = 'Apache 2.0'
+
+import copy
+import re
+import ntplib
+
+from mi.core.log import get_logger ; log = get_logger()
+from mi.core.common import BaseEnum
+from mi.core.instrument.data_particle import DataParticle, DataParticleKey
+from mi.core.exceptions import SampleException, DatasetParserException, UnexpectedDataException
+
+from mi.dataset.dataset_parser import BufferLoadingParser
+
+# *** Need to define data regex for this parser ***
+DATA_REGEX = ''
+DATA_MATCHER = re.compile(DATA_REGEX)
+
+class DataParticleType(BaseEnum):
+    SAMPLE = 'vel3d_l_wfp_parsed'
+
+class Vel3dLWfpParserDataParticleKey(BaseEnum):
+
+class StateKey(BaseEnum):
+    POSITION='position' # holds the file position
+
+class Vel3dLWfpParserDataParticle(DataParticle):
+    """
+    Class for parsing data from the vel3d_l_wfp data set
+    """
+
+    _data_particle_type = DataParticleType.SAMPLE
+    
+    def _build_parsed_values(self):
+        """
+        Take something in the data format and turn it into
+        an array of dictionaries defining the data in the particle
+        with the appropriate tag.
+        @throws SampleException If there is a problem with sample creation
+        """
+        pass
+
+class Vel3dLWfpParser(BufferLoadingParser):
+
+    def set_state(self, state_obj):
+        """
+        Set the value of the state object for this parser
+        @param state_obj The object to set the state to. 
+        @throws DatasetParserException if there is a bad state structure
+        """
+        if not isinstance(state_obj, dict):
+            raise DatasetParserException("Invalid state structure")
+        self._timestamp = state_obj[StateKey.TIMESTAMP]
+        self._state = state_obj
+        self._read_state = state_obj
+
+    def _increment_state(self, increment):
+        """
+        Increment the parser state
+        @param timestamp The timestamp completed up to that position
+        """
+        self._read_state[StateKey.POSITION] += increment
+
+    def parse_chunks(self):
+        """
+        Parse out any pending data chunks in the chunker. If
+        it is a valid data piece, build a particle, update the position and
+        timestamp. Go until the chunker has no more valid data.
+        @retval a list of tuples with sample particles encountered in this
+            parsing, plus the state. An empty list of nothing was parsed.
+        """            
+        result_particles = []
+        (nd_timestamp, non_data, non_start, non_end) = self._chunker.get_next_non_data_with_index(clean=False)
+        (timestamp, chunk, start, end) = self._chunker.get_next_data_with_index(clean=True)
+        self.handle_non_data(non_data, non_end, start)
+
+        while (chunk != None):
+            data_match = DATA_MATCHER.match(chunk)
+            if data_match:
+                # particle-ize the data block received, return the record
+                sample = self._extract_sample(self._particle_class, DATA_MATCHER, chunk, self._timestamp)
+                if sample:
+                    # create particle
+                    log.trace("Extracting sample chunk %s with read_state: %s", chunk, self._read_state)
+                    self._increment_state(len(chunk))
+                    result_particles.append((sample, copy.copy(self._read_state)))
+
+            (nd_timestamp, non_data, non_start, non_end) = self._chunker.get_next_non_data_with_index(clean=False)
+            (timestamp, chunk, start, end) = self._chunker.get_next_data_with_index(clean=True)
+            self.handle_non_data(non_data, non_end, start)
+
+        return result_particles
+
+    def handle_non_data(self, non_data, non_end, start):
+        """
+        Handle any non-data that is found in the file
+        """
+        # if non-data is expected, handle it here, otherwise it is an error
+        if non_data is not None and non_end <= start:
+            # if this non-data is an error, send an UnexpectedDataException and increment the state
+            self._increment_state(len(non_data))
+            # if non-data is a fatal error, directly call the exception, if it is not use the _exception_callback
+            self._exception_callback(UnexpectedDataException("Found %d bytes of un-expected non-data %s" % (len(non_data), non_data)))
