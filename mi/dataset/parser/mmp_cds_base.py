3da75ee73b51aab16ac5d76e1936673598524f67
Jeremy Amundson <jeremy.s.amundson@raytheon.com>
2014-06-26 07:03:12 -0700

    flntu_x and flcdr_x  mmp_cds parsers for review

---
+++ mi/dataset/parser/mmp_cds_base.py
@@ -0,0 +1,342 @@
+
+#!/usr/bin/env python
+
+"""
+@package mi.dataset.parser.ctdpf_ckl_mmp_cds
+@file marine-integrations/mi/dataset/parser/mmp_cds_base.py
+@author Mark Worden
+@brief Base Parser for the MmpCds dataset drivers
+Release notes:
+
+initial release
+"""
+
+__author__ = 'Mark Worden'
+__license__ = 'Apache 2.0'
+
+import copy
+import gevent
+import msgpack
+import ntplib
+import time
+
+from mi.core.log import get_logger
+
+log = get_logger()
+from mi.core.common import BaseEnum
+from mi.core.instrument.data_particle import DataParticle
+from mi.core.exceptions import DatasetParserException, SampleException
+from mi.dataset.dataset_driver import DataSetDriverConfigKeys
+from mi.dataset.dataset_parser import BufferLoadingParser
+
+# The number of items in a list associated unpackaed data within a McLane Moored Profiler cabled docking station
+# data chunk
+NUM_MMP_CDS_UNPACKED_ITEMS = 3
+
+
+class StateKey(BaseEnum):
+    POSITION = 'position' # holds the file position
+    PARTICLES_RETURNED = 'particles_returned' # holds the number of particles returned
+
+
+class MmpCdsParserDataParticleKey(BaseEnum):
+    RAW_TIME_SECONDS = 'raw_time_seconds'
+    RAW_TIME_MICROSECONDS = 'raw_time_microseconds'
+
+
+class MmpCdsParserDataParticle(DataParticle):
+    """
+Class for building a data particle given parsed data as received from a McLane Moored Profiler connected to
+a cabled docking station.
+"""
+
+    def _get_mmp_cds_subclass_particle_params(self, dict_data):
+        """
+This method is expected to be implemented by subclasses. It is okay to let the implemented method to
+allow the following exceptions to propagate: ValueError, TypeError, IndexError
+@param dict_data the dictionary data containing the specific particle parameter name value pairs
+@return a list of particle params specific to the subclass
+"""
+
+        # This implementation raises a NotImplemented exception to enforce derived classes to implement
+        # this method.
+        raise NotImplemented
+
+    def _build_parsed_values(self):
+        """
+This method generates a list of particle parameters using the self.raw_data which is expected to be
+a list of three items. The first item is expected to be the "raw_time_seconds". The second item
+is expected to be the "raw_time_microseconds". The third item is expected to be a dictionary. This
+method depends on an abstract method (_get_mmp_cds_subclass_particle_params) to generate the specific
+particle parameters from the third list item dictionary content.
+@throws SampleException If there is a problem with sample creation
+"""
+        try:
+
+            raw_time_seconds = self._encode_value(MmpCdsParserDataParticleKey.RAW_TIME_SECONDS,
+                                                  self.raw_data[0], int)
+            raw_time_microseconds = self._encode_value(MmpCdsParserDataParticleKey.RAW_TIME_MICROSECONDS,
+                                                       self.raw_data[1], int)
+
+            subclass_particle_params = self._get_mmp_cds_subclass_particle_params(self.raw_data[2])
+
+        except (ValueError, TypeError, IndexError) as ex:
+            log.debug("Raising SampleException as a result of unexpected msgpack data")
+            raise SampleException("Error (%s) while decoding parameters in data: [%s]"
+                                  % (ex, self.raw_data))
+
+        result = [raw_time_seconds,
+                  raw_time_microseconds] + subclass_particle_params
+
+        log.debug('MmpCdsParserDataParticle: particle=%s', result)
+        return result
+
+
+class MmpCdsParser(BufferLoadingParser):
+    """
+Class for parsing data as received from a McLane Moored Profiler connected to a cabled docking station.
+"""
+
+    def __init__(self,
+                 config,
+                 state,
+                 stream_handle,
+                 state_callback,
+                 publish_callback,
+                 *args, **kwargs):
+        """
+This method is a constructor that will instantiate an MmpCdsParser object.
+@param config The configuration for this MmpCdsParser parser
+@param state The state the MmpCdsParser should use to initialize itself
+@param stream_handle The handle to the data stream containing the MmpCds data
+@param state_callback The function to call upon detecting state changes
+@param publish_callback The function to call to provide particles
+"""
+
+        # Initialize the record buffer to an empty list
+        self._record_buffer = []
+
+        # Initialize the read state to the POSITION being 0
+        self._read_state = {StateKey.POSITION: 0, StateKey.PARTICLES_RETURNED: 0}
+
+        # Pop off the kwargs the key value pairs associated with the DataSetDriverConfigKeys.PARTICLE_CLASS key
+        self._particle_class = kwargs.pop(DataSetDriverConfigKeys.PARTICLE_CLASS)
+
+        # Call the superclass constructor
+        super(MmpCdsParser, self).__init__(config,
+                                           stream_handle,
+                                           state,
+                                           self.sieve_function,
+                                           state_callback,
+                                           publish_callback,
+                                           *args, **kwargs)
+
+        # If provided a state, set it. This needs to be done post superclass __init__
+        if state:
+            self.set_state(state)
+
+    def set_state(self, state_obj):
+        """
+This method will set or re-set the state of the MmpCdsParser to a given state
+@param state_obj the updated state to use
+"""
+        log.debug("Attempting to set state to: %s", state_obj)
+        # First need to make sure the state type is a dict
+        if not isinstance(state_obj, dict):
+            log.debug("Invalid state structure")
+            raise DatasetParserException("Invalid state structure")
+        # Then we need to make sure that the provided state includes position information
+        if not (StateKey.POSITION in state_obj):
+            log.debug("Invalid state keys")
+            raise DatasetParserException("Invalid state keys")
+
+        # Clear out any pre-existing chunks
+        self._chunker.clean_all_chunks()
+
+        self._record_buffer = []
+
+        # Set the state and read state to the provide state
+        self._state = state_obj
+        self._read_state = state_obj
+
+        # Always seek to the beginning of the buffer to read all records
+        self._stream_handle.seek(0)
+
+    def _yank_particles(self, num_records):
+        """
+Get particles out of the buffer and publish them. Update the state
+of what has been published, too.
+@param num_records The number of particles to remove from the buffer
+@retval A list with num_records elements from the buffer. If num_records
+cannot be collected (perhaps due to an EOF), the list will have the
+elements it was able to collect.
+"""
+        particles_returned = 0
+
+        if self._state is not None and StateKey.PARTICLES_RETURNED in self._state and \
+                self._state[StateKey.PARTICLES_RETURNED] > 0:
+            particles_returned = self._state[StateKey.PARTICLES_RETURNED]
+
+        total_num_records = len(self._record_buffer)
+        log.info(total_num_records)
+        log.info(particles_returned)
+
+        if total_num_records < num_records:
+            num_to_fetch = len(self._record_buffer)
+        else:
+            num_to_fetch = num_records
+
+        log.debug("Yanking %s records of %s requested",
+                  num_to_fetch,
+                  num_records)
+
+        return_list = []
+
+        end_range = particles_returned + num_to_fetch
+
+        records_to_return = self._record_buffer[particles_returned:end_range]
+        if len(records_to_return) > 0:
+            # Get the state of the last tuple entry
+            self._state = records_to_return[-1][1]
+
+            # Update the number of particles returned
+            self._state[StateKey.PARTICLES_RETURNED] = particles_returned+num_to_fetch
+            self._read_state = self._state
+
+            # strip the state info off of them now that we have what we need
+            for item in records_to_return:
+                log.debug("Record to return: %s", item)
+                return_list.append(item[0])
+
+            self._publish_sample(return_list)
+            log.trace("Sending parser state [%s] to driver", self._state)
+            file_ingested = False
+            if self.file_complete and total_num_records == self._state[StateKey.PARTICLES_RETURNED]:
+                # file has been read completely and all records pulled out of the record buffer
+                file_ingested = True
+            self._state_callback(self._state, file_ingested) # push new state to driver
+
+        return return_list
+
+    def _set_position(self, position):
+        """
+Increment the parser state (i.e. position into the read state)
+@param increment The updated offset into the read state
+"""
+        self._read_state[StateKey.POSITION] = position
+
+    def get_block(self, size=1024):
+        """
+This function overrides the get_block function in BufferLoadingParser
+to read the entire file rather than break it into chunks.
+@return The length of data retrieved.
+@throws EOFError when the end of the file is reached.
+"""
+        # Read in data in blocks so as to not tie up the CPU.
+        eof = False
+        data = ''
+        while not eof:
+            next_block = self._stream_handle.read(size)
+            if next_block:
+                data = data + next_block
+                gevent.sleep(0)
+            else:
+                eof = True
+
+        if data != '':
+            self._timestamp = float(ntplib.system_to_ntp_time(time.time()))
+            log.info("Calculated current time timestamp %.10f", self._timestamp)
+            self._chunker.add_chunk(data, self._timestamp)
+            self.file_complete = True
+            return len(data)
+        else: # EOF
+            self.file_complete = True
+            raise EOFError
+
+    def sieve_function(self, raw_data):
+        """
+This method sorts through the raw data to identify new blocks of data that need processing. This method
+identifies the start index as 0 and the length of the input raw_data as the end.
+@param raw_data the raw msgpack data for which to return the chunk location information
+@return the list of tuples containing the start index and range for each chunk
+"""
+
+        # The raw_data provided as input is considered the full recovered file byte stream, and will be
+        # considered a single chunk. In a file containing msgpack serialized data, there are not multiple
+        # headers and records.
+        return [(0, len(raw_data))]
+
+    def parse_chunks(self):
+        """
+This method parses each chunk and attempts to extract samples to return.
+@return for each discovered sample, a list of tuples containing each particle and associated state position
+# information
+"""
+        # Initialize the resultant particle list to return to an emtpy list
+        result_particles = []
+
+        # Obtain the next chunk to process
+        (timestamp, chunk, start, end) = self._chunker.get_next_data_with_index(clean=True)
+
+        # We need to use the msgpack library and instantiate an Unpacker to process the chunk of data
+        unpacker = msgpack.Unpacker()
+
+        # Process each chunk as long as one exists
+        if chunk is not None:
+
+            # Feed the Unpacker instance the chunk of data
+            unpacker.feed(chunk)
+
+            # Initialize the list of samples for this chunk to an emtpy list
+            samples = []
+
+            # We need to put the following in a try block just in case the chunk of data provided is malformed
+            try:
+
+                # Let's iterate through each unpacked list item
+                for unpacked_data in unpacker:
+                    # The expectation is that an unpacked list item associated with a McLane Moored Profiler cabled
+                    # docking station data chunk consists of a list of three items
+                    istuple = isinstance(unpacked_data, tuple) or isinstance(unpacked_data, list)
+                    log.debug("unpacked data is a tuple or list %d", istuple)
+                    log.debug("the length is %d", len(unpacked_data))
+                    if isinstance(unpacked_data, tuple) or isinstance(unpacked_data, list) and \
+                            len(unpacked_data) == NUM_MMP_CDS_UNPACKED_ITEMS:
+                        # Attempt to convert the raw time in seconds (unpacked_data[0]) and raw time in microseconds
+                        # (unpacked_data[1]) to an NTP 64 timestamp
+                        timestamp = ntplib.system_to_ntp_time(unpacked_data[0] + unpacked_data[1]/1000000.0)
+
+                        log.info("Calculated timestamp from raw %.10f", timestamp)
+
+                        # Extract the sample an provide the particle class which could be different for each
+                        # derived MmpCdsParser
+                        sample = self._extract_sample(self._particle_class, None, unpacked_data, timestamp)
+
+                        # If we extracted a sample, add it to the list of samples to return
+                        if sample:
+                            samples.append(sample)
+
+                    else:
+                        log.debug("Raising SampleException as a result of a badly formed msgpack file")
+                        raise SampleException("Invalid ctdpf_ckl_mmp_cds msgpack contents")
+
+                    # Let's call gevent.sleep with 0 to allow for the CPU to be used by another gevent thread just
+                    # in case we are dealing with a large list of unpacked msgpack data
+                    gevent.sleep(0)
+
+            except TypeError:
+                log.debug("Raising SampleException as a result of not being able to iterate through the "
+                          "unpacked msgpack data")
+                raise SampleException("Invalid ctdpf_ckl_mmp_cds msgpack contents")
+
+            # We're done with this chunk, let's offset the state
+            self._set_position(len(chunk))
+
+            # For each sample we retrieved in the chunk, let's create a tuple containing the sample, and the parser's
+            # current read state
+            for sample in samples:
+                result_particles.append((sample, copy.copy(self._read_state)))
+
+        return result_particles
+
+
---
+++ mi/dataset/parser/test/test_flcdr_x_mmp_cds.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python
+
+"""
+@package mi.dataset.parser.test.test_flcdr_x_mmp_cds
+@file marine-integrations/mi/dataset/parser/test/test_flcdr_x_mmp_cds.py
+@author Jeremy Amundson
+@brief Test code for a flcdr_x_mmp_cds data parser
+Since the parser is to be merged with flntu_x_mmp_cds the files used in
+this test
+"""
+
+import os
+import numpy
+import yaml
+
+from nose.plugins.attrib import attr
+
+from mi.core.log import get_logger
+from mi.idk.config import Config
+
+log = get_logger()
+from mi.core.exceptions import SampleException
+
+from mi.dataset.test.test_parser import ParserUnitTestCase
+from mi.dataset.dataset_driver import DataSetDriverConfigKeys
+from mi.dataset.parser.flcdr_x_mmp_cds import FlcdrXMmpCdsParser
+from mi.dataset.parser.mmp_cds_base import StateKey
+
+# Resource path for flcdr x mmp cds
+RESOURCE_PATH = os.path.join(Config().base_dir(), 'mi', 'dataset', 'driver', 'flntu_x', 'mmp_cds', 'resource')
+
+# The list of generated tests are the suggested tests, but there may
+# be other tests needed to fully test your parser
+
+@attr('UNIT', group='mi')
+class FlcdrXMmpCdsParserUnitTestCase(ParserUnitTestCase):
+    """
+    flcdr_x_mmp_cds Parser unit test suite
+    """
+
+    def state_callback(self, state, file_ingested):
+        """ Call back method to watch what comes in via the position callback """
+        self.state_callback_value = state
+        self.file_ingested_value = file_ingested
+
+    def pub_callback(self, pub):
+        """ Call back method to watch what comes in via the publish callback """
+        self.publish_callback_value = pub
+
+    def setUp(self):
+        ParserUnitTestCase.setUp(self)
+        self.config = {
+            DataSetDriverConfigKeys.PARTICLE_MODULE: 'mi.dataset.parser.flcdr_x_mmp_cds',
+            DataSetDriverConfigKeys.PARTICLE_CLASS: 'FlcdrXMmpCdsParserDataParticle'
+        }
+
+        # Define test data particles and their associated timestamps which will be
+        # compared with returned results
+        self.file_ingested_value = None
+        self.state_callback_value = None
+        self.publish_callback_value = None
+
+        self.start_state = {StateKey.POSITION: 0}
+
+    def test_simple(self):
+        """
+        This test reads in a small number of particles and verifies the result of one of the particles.
+        """
+
+        file_path = os.path.join(RESOURCE_PATH, 'flcdr_1_20131124T005004_458.mpk')
+        stream_handle = open(file_path, 'rb')
+
+        parser = FlcdrXMmpCdsParser(self.config, self.start_state, stream_handle,
+                                      self.state_callback, self.pub_callback)
+
+        particles = parser.get_records(6)
+
+        for particle in particles:
+            print particle.generate_dict()
+
+        test_data = self.get_dict_from_yml('good_cdr.yml')
+        self.assert_result(test_data['data'][0], particles[5])
+
+        stream_handle.close()
+
+    def test_get_many(self):
+        """
+        This test exercises retrieving 20 particles, verifying the 20th particle, then retrieves 30 particles
+         and verifies the 30th particle.
+        """
+
+        file_path = os.path.join(RESOURCE_PATH, 'flcdr_1_20131124T005004_458.mpk')
+        stream_handle = open(file_path, 'rb')
+
+        self.parser = FlcdrXMmpCdsParser(self.config, self.start_state, stream_handle,
+                                           self.state_callback, self.pub_callback)
+
+        particles = self.parser.get_records(20)
+
+        # Should end up with 20 particles
+        self.assertTrue(len(particles) == 20)
+
+        test_data = self.get_dict_from_yml('get_many_one_cdr.yml')
+        self.assert_result(test_data['data'][0], particles[19])
+
+        particles = self.parser.get_records(30)
+
+        # Should end up with 30 particles
+        self.assertTrue(len(particles) == 30)
+
+        test_data = self.get_dict_from_yml('get_many_two_cdr.yml')
+        self.assert_result(test_data['data'][0], particles[29])
+
+        stream_handle.close()
+
+    def test_long_stream(self):
+        """
+        This test exercises retrieve approximately 200 particles.
+        """
+
+        # Using two concatenated msgpack files to simulate two chunks to get more particles.
+        file_path = os.path.join(RESOURCE_PATH, 'flcdr_concat.mpk')
+        stream_handle = open(file_path, 'rb')
+
+        self.parser = FlcdrXMmpCdsParser(self.config, self.start_state, stream_handle,
+                                           self.state_callback, self.pub_callback)
+
+        # Attempt to retrieve 400 particles, but we will retrieve less
+        particles = self.parser.get_records(400)
+
+        log.info(len(particles))
+
+        # Should end up with 394 particles
+        self.assertTrue(len(particles) == 394)
+
+        stream_handle.close()
+
+    def test_mid_state_start(self):
+        """
+        This test exercises setting the state past one chunk, retrieving particles and verify the result of one
+        of the particles.
+        """
+
+        # Using two concatenated msgpack files to simulate two chunks.
+        file_path = os.path.join(RESOURCE_PATH, 'flcdr_concat.mpk')
+        stream_handle = open(file_path, 'rb')
+
+        # Moving the file position to the end of the first chunk
+        new_state = {StateKey.POSITION: 5278}
+
+        self.parser = FlcdrXMmpCdsParser(self.config, new_state, stream_handle,
+                                           self.state_callback, self.pub_callback)
+
+        particles = self.parser.get_records(4)
+
+        log.info(len(particles))
+
+        # Should end up with 4 particles
+        self.assertTrue(len(particles) == 4)
+
+        test_data = self.get_dict_from_yml('mid_state_start_cdr.yml')
+        self.assert_result(test_data['data'][0], particles[3])
+
+        stream_handle.close()
+
+    def test_set_state(self):
+        """
+        This test exercises setting the state past one chunk, retrieving particles, verifying one
+        of the particles, and then setting the state back to the begging, retrieving a few particles, and
+        verifying one of the particles.
+        """
+
+        # Using two concatenated msgpack files to simulate two chunks.
+        filepath = os.path.join(RESOURCE_PATH, 'flcdr_concat.mpk')
+        stream_handle = open(filepath, 'rb')
+
+        # Moving the file position past the first chunk
+        new_state = {StateKey.POSITION: 2752}
+
+        self.parser = FlcdrXMmpCdsParser(self.config, new_state, stream_handle,
+                                         self.state_callback, self.pub_callback)
+
+        particles = self.parser.get_records(4)
+
+        # Should end up with 4 particles
+        self.assertTrue(len(particles) == 4)
+
+        test_data = self.get_dict_from_yml('mid_state_start_cdr.yml')
+        self.assert_result(test_data['data'][0], particles[3])
+
+        # Moving the file position past the header and three records
+        new_state = {StateKey.POSITION: 0}
+
+        self.parser = FlcdrXMmpCdsParser(self.config, new_state, stream_handle,
+                                         self.state_callback, self.pub_callback)
+
+        particles = self.parser.get_records(20)
+
+        # Should end up with 20 particles
+        self.assertTrue(len(particles) == 20)
+
+        test_data = self.get_dict_from_yml('get_many_one_cdr.yml')
+        self.assert_result(test_data['data'][0], particles[19])
+
+        stream_handle.close()
+
+    def test_bad_data_one(self):
+        """
+        This test verifies that a SampleException is raised when msgpack data is malformed.
+        """
+
+        file_path = os.path.join(RESOURCE_PATH, 'flcdr_1_20131124T005004_458-BAD.mpk')
+        stream_handle = open(file_path, 'rb')
+
+        parser = FlcdrXMmpCdsParser(self.config, self.start_state, stream_handle,
+                                    self.state_callback, self.pub_callback)
+
+        with self.assertRaises(SampleException):
+            parser.get_records(1)
+
+        stream_handle.close()
+
+    def test_bad_data_two(self):
+        """
+        This test verifies that a SampleException is raised when an entire msgpack buffer is not msgpack.
+        """
+
+        file_path = os.path.join(RESOURCE_PATH, 'not-msg-pack.mpk')
+        stream_handle = open(file_path, 'rb')
+
+        parser = FlcdrXMmpCdsParser(self.config, self.start_state, stream_handle,
+                                    self.state_callback, self.pub_callback)
+
+        with self.assertRaises(SampleException):
+            parser.get_records(1)
+
+        stream_handle.close()
+
+    def assert_result(self, test, particle):
+        """
+        Suite of tests to run against each returned particle and expected
+        results of the same.  The test parameter should be a dictionary
+        that contains the keys to be tested in the particle
+        the 'internal_timestamp' and 'position' keys are
+        treated differently than others but can be verified if supplied
+        """
+
+        particle_dict = particle.generate_dict()
+
+        #for efficiency turn the particle values list of dictionaries into a dictionary
+        particle_values = {}
+        for param in particle_dict.get('values'):
+            particle_values[param['value_id']] = param['value']
+
+        # compare each key in the test to the data in the particle
+        for key in test:
+            test_data = test[key]
+
+            #get the correct data to compare to the test
+            if key == 'internal_timestamp':
+                particle_data = particle.get_value('internal_timestamp')
+                #the timestamp is in the header part of the particle
+
+                log.info("internal_timestamp %.10f", particle_data)
+
+            elif key == 'position':
+                particle_data = self.state_callback_value['position']
+                #position corresponds to the position in the file
+
+                log.info("position %d", particle_data)
+
+            else:
+                particle_data = particle_values.get(key)
+                #others are all part of the parsed values part of the particle
+
+            if particle_data is None:
+                #generally OK to ignore index keys in the test data, verify others
+
+                log.warning("\nWarning: assert_result ignoring test key %s, does not exist in particle", key)
+            else:
+                log.info(key)
+                log.info(type(test_data))
+                if isinstance(test_data, float):
+                    # slightly different test for these values as they are floats.
+                    compare = numpy.abs(test_data - particle_data) <= 1e-5
+                    self.assertTrue(compare)
+                else:
+                    # otherwise they are all ints and should be exactly equal
+                    self.assertEqual(test_data, particle_data)
+
+    def get_dict_from_yml(self, filename):
+        """
+        This utility routine loads the contents of a yml file
+        into a dictionary
+        """
+
+        fid = open(os.path.join(RESOURCE_PATH, filename), 'r')
+        result = yaml.load(fid)
+        fid.close()
+
+        return result
