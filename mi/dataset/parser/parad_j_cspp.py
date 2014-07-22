acff8d854a4aa3c6a746f25610a6e39307b468f4
Joseph J Padula <Joseph_J_Padula@raytheon.com>
2014-07-22 12:09:29 -0700

    parad_j_parser coding complete.

--- mi/dataset/parser/parad_j_cspp.py
+++ mi/dataset/parser/parad_j_cspp.py
@@ -13,36 +13,93 @@ initial release
 __author__ = 'Joe Padula'
 __license__ = 'Apache 2.0'
 
-import copy
 import re
-import ntplib
+import numpy
+import string
 
-from mi.core.log import get_logger ; log = get_logger()
+from mi.core.log import get_logger
+log = get_logger()
 from mi.core.common import BaseEnum
 from mi.core.instrument.data_particle import DataParticle, DataParticleKey
 from mi.core.exceptions import SampleException, DatasetParserException, UnexpectedDataException
 
-from mi.dataset.dataset_parser import BufferLoadingParser
+from mi.dataset.parser.cspp_base import \
+    CsppParser, \
+    FLOAT_REGEX, \
+    INT_REGEX, \
+    Y_OR_N_REGEX, \
+    MULTIPLE_TAB_REGEX, \
+    CARRIAGE_RETURN_LINE_FEED_OR_BOTH, \
+    CsppMetadataDataParticle, \
+    MetadataRawDataKey, \
+    encode_y_or_n
+
+# Date is in format MM/DD/YY, example 04/17/14
+DATE_REGEX = r'(\d{2}/\d{2}/\d{2})'
+# Time is in format HH:MM:SS, example 15:22:31
+TIME_REGEX = r'(\d{2}:\d{2}:\d{2})'
 
 # *** Need to define data regex for this parser ***
-DATA_REGEX = ''
-DATA_MATCHER = re.compile(DATA_REGEX)
+DATA_REGEX = r'(' + FLOAT_REGEX + ')' + MULTIPLE_TAB_REGEX  # Profiler Timestamp
+DATA_REGEX += '(' + FLOAT_REGEX + ')' + MULTIPLE_TAB_REGEX  # Depth
+DATA_REGEX += '(' + Y_OR_N_REGEX + ')' + MULTIPLE_TAB_REGEX     # Suspect Timestamp
+DATA_REGEX += '(' + DATE_REGEX + ')' + MULTIPLE_TAB_REGEX     # Date
+DATA_REGEX += '(' + TIME_REGEX + ')' + MULTIPLE_TAB_REGEX     # Time
+DATA_REGEX += '(' + INT_REGEX + ')' + CARRIAGE_RETURN_LINE_FEED_OR_BOTH    # par
+
+# IDD states the configuration rows after the header as well as occasional malformed data rows
+# can be ignored.
+#
+# Ignore any rows that begin with the timestamp and depth but
+# do not match the data record or the header rows formats
+IGNORE_REGEX = FLOAT_REGEX + MULTIPLE_TAB_REGEX     # Profiler Timestamp
+IGNORE_REGEX += FLOAT_REGEX + MULTIPLE_TAB_REGEX    # Depth
+IGNORE_REGEX += '(' + Y_OR_N_REGEX + ')' + MULTIPLE_TAB_REGEX     # Suspect Timestamp
+IGNORE_REGEX += '.*' + CARRIAGE_RETURN_LINE_FEED_OR_BOTH    # any text after the Depth
+
+IGNORE_MATCHER = re.compile(IGNORE_REGEX)
+
+
+class DataMatchesGroupNumber(BaseEnum):
+    """
+    An enum for group match indices for a data record chunk.
+    Used to access the match groups in the particle raw data
+    """
+    PROFILER_TIMESTAMP = 1
+    DEPTH = 2
+    SUSPECT_TIMESTAMP = 3
+    DATE = 4
+    TIME = 5
+    PAR = 6
+
 
 class DataParticleType(BaseEnum):
-    SAMPLE = 'parad_j_cspp_parsed'
+    """
+    The data particle types that a dosta_abcdjm_cspp parser could generate
+    """
+    METADATA_RECOVERED = 'parad_j_cspp_metadata_recovered'
+    INSTRUMENT_RECOVERED = 'parad_j_cspp_instrument_recovered'
+    METADATA_TELEMETERED = 'parad_j_cspp_metadata'
+    INSTRUMENT_TELEMETERED = 'parad_j_cspp_instrument'
+
 
 class ParadJCsppParserDataParticleKey(BaseEnum):
-
-class StateKey(BaseEnum):
-    POSITION='position' # holds the file position
-
-class ParadJCsppParserDataParticle(DataParticle):
     """
-    Class for parsing data from the parad_j_cspp data set
+    The data particle keys associated with parad_j_cspp data instrument particle parameters
+    """
+    PROFILER_TIMESTAMP = 'profiler_timestamp'
+    PRESSURE_DEPTH = 'pressure_depth'
+    SUSPECT_TIMESTAMP = 'suspect_timestamp'
+    DATE_STRING = 'date_string'
+    TIME_STRING = 'time_string'
+    PAR = 'par'
+
+
+class ParadJCsppMetadataDataParticle(CsppMetadataDataParticle):
+    """
+    Base Class for building a parad_j_cspp metadata particle
     """
 
-    _data_particle_type = DataParticleType.SAMPLE
-    
     def _build_parsed_values(self):
         """
         Take something in the data format and turn it into
@@ -50,66 +107,143 @@ class ParadJCsppParserDataParticle(DataParticle):
         with the appropriate tag.
         @throws SampleException If there is a problem with sample creation
         """
-        pass
 
-class ParadJCsppParser(BufferLoadingParser):
+        results = []
 
-    def set_state(self, state_obj):
+        try:
+
+            # Append the base metadata parsed values to the results to return
+            results += self._build_metadata_parsed_values()
+
+            data_match = self.raw_data[MetadataRawDataKey.DATA_MATCH]
+
+            # Set the internal timestamp
+            internal_timestamp_unix = numpy.float(data_match.group(
+                DataMatchesGroupNumber.PROFILER_TIMESTAMP))
+            self.set_internal_timestamp(unix_time=internal_timestamp_unix)
+
+        except (ValueError, TypeError, IndexError) as ex:
+            log.warn("Exception when building parsed values")
+            raise SampleException("Error (%s) while decoding parameters in data: [%s]"
+                                  % (ex, self.raw_data))
+
+        log.debug('*** metadata particle result %s', results)
+
+        return results
+
+
+class ParadJCsppMetadataRecoveredDataParticle(ParadJCsppMetadataDataParticle):
+    """
+    Class for building a parad_j_cspp recovered metadata particle
+    """
+
+    _data_particle_type = DataParticleType.METADATA_RECOVERED
+
+
+class ParadJCsppMetadataTelemeteredDataParticle(ParadJCsppMetadataDataParticle):
+    """
+    Class for building a parad_j_cspp telemetered metadata particle
+    """
+
+    _data_particle_type = DataParticleType.METADATA_TELEMETERED
+
+
+class ParadJCsppInstrumentDataParticle(DataParticle):
+    """
+    Base Class for building a parad_j_cspp instrument data particle
+    """
+
+    def _build_parsed_values(self):
         """
-        Set the value of the state object for this parser
-        @param state_obj The object to set the state to. 
-        @throws DatasetParserException if there is a bad state structure
+        Take something in the data format and turn it into
+        an array of dictionaries defining the data in the particle
+        with the appropriate tag.
+        @throws SampleException If there is a problem with sample creation
         """
-        if not isinstance(state_obj, dict):
-            raise DatasetParserException("Invalid state structure")
-        self._timestamp = state_obj[StateKey.TIMESTAMP]
-        self._state = state_obj
-        self._read_state = state_obj
+        results = []
 
-    def _increment_state(self, increment):
+        try:
+            results.append(self._encode_value(ParadJCsppParserDataParticleKey.PROFILER_TIMESTAMP,
+                                              self.raw_data.group(DataMatchesGroupNumber.PROFILER_TIMESTAMP),
+                                              numpy.float))
+
+            results.append(self._encode_value(ParadJCsppParserDataParticleKey.PRESSURE_DEPTH,
+                                              self.raw_data.group(DataMatchesGroupNumber.PRESSURE_DEPTH),
+                                              float))
+
+            results.append(self._encode_value(ParadJCsppParserDataParticleKey.SUSPECT_TIMESTAMP,
+                                              self.raw_data.group(DataMatchesGroupNumber.SUSPECT_TIMESTAMP),
+                                              encode_y_or_n))
+
+            results.append(self._encode_value(ParadJCsppParserDataParticleKey.DATE_STRING,
+                                              self.raw_data.group(DataMatchesGroupNumber.DATE),
+                                              string))
+
+            results.append(self._encode_value(ParadJCsppParserDataParticleKey.TIME_STRING,
+                                              self.raw_data.group(DataMatchesGroupNumber.TIME),
+                                              string))
+
+            results.append(self._encode_value(ParadJCsppParserDataParticleKey.PAR,
+                                              self.raw_data.group(DataMatchesGroupNumber.PAR),
+                                              int))
+
+            # # Set the internal timestamp
+            internal_timestamp_unix = numpy.float(self.raw_data.group(
+                DataMatchesGroupNumber.PROFILER_TIMESTAMP))
+            self.set_internal_timestamp(unix_time=internal_timestamp_unix)
+
+        except (ValueError, TypeError, IndexError) as ex:
+            log.warn("Exception when building parsed values")
+            raise SampleException("Error (%s) while decoding parameters in data: [%s]"
+                                  % (ex, self.raw_data))
+
+        log.debug('*** instrument particle result %s', results)
+        return results
+
+
+class ParadJCsppInstrumentRecoveredDataParticle(ParadJCsppInstrumentDataParticle):
+    """
+    Class for building a parad_j_cspp recovered instrument data particle
+    """
+
+    _data_particle_type = DataParticleType.INSTRUMENT_RECOVERED
+
+
+class ParadJCsppInstrumentTelemeteredDataParticle(ParadJCsppInstrumentDataParticle):
+    """
+    Class for building a parad_j_cspp telemetered instrument data particle
+    """
+
+    _data_particle_type = DataParticleType.INSTRUMENT_TELEMETERED
+
+
+class ParadJCsppParser(CsppParser):
+
+    def __init__(self,
+                 config,
+                 state,
+                 stream_handle,
+                 state_callback,
+                 publish_callback,
+                 exception_callback,
+                 *args, **kwargs):
         """
-        Increment the parser state
-        @param timestamp The timestamp completed up to that position
+        This method is a constructor that will instantiate an ParadJCsppParser object.
+        @param config The configuration for this ParadJCsppParser parser
+        @param state The state the ParadJCsppParser should use to initialize itself
+        @param stream_handle The handle to the data stream containing the parad_j_cspp data
+        @param state_callback The function to call upon detecting state changes
+        @param publish_callback The function to call to provide particles
+        @param exception_callback The function to call to report exceptions
         """
-        self._read_state[StateKey.POSITION] += increment
 
-    def parse_chunks(self):
-        """
-        Parse out any pending data chunks in the chunker. If
-        it is a valid data piece, build a particle, update the position and
-        timestamp. Go until the chunker has no more valid data.
-        @retval a list of tuples with sample particles encountered in this
-            parsing, plus the state. An empty list of nothing was parsed.
-        """            
-        result_particles = []
-        (nd_timestamp, non_data, non_start, non_end) = self._chunker.get_next_non_data_with_index(clean=False)
-        (timestamp, chunk, start, end) = self._chunker.get_next_data_with_index(clean=True)
-        self.handle_non_data(non_data, non_end, start)
-
-        while (chunk != None):
-            data_match = DATA_MATCHER.match(chunk)
-            if data_match:
-                # particle-ize the data block received, return the record
-                sample = self._extract_sample(self._particle_class, DATA_MATCHER, chunk, self._timestamp)
-                if sample:
-                    # create particle
-                    log.trace("Extracting sample chunk %s with read_state: %s", chunk, self._read_state)
-                    self._increment_state(len(chunk))
-                    result_particles.append((sample, copy.copy(self._read_state)))
-
-            (nd_timestamp, non_data, non_start, non_end) = self._chunker.get_next_non_data_with_index(clean=False)
-            (timestamp, chunk, start, end) = self._chunker.get_next_data_with_index(clean=True)
-            self.handle_non_data(non_data, non_end, start)
-
-        return result_particles
-
-    def handle_non_data(self, non_data, non_end, start):
-        """
-        Handle any non-data that is found in the file
-        """
-        # if non-data is expected, handle it here, otherwise it is an error
-        if non_data is not None and non_end <= start:
-            # if this non-data is an error, send an UnexpectedDataException and increment the state
-            self._increment_state(len(non_data))
-            # if non-data is a fatal error, directly call the exception, if it is not use the _exception_callback
-            self._exception_callback(UnexpectedDataException("Found %d bytes of un-expected non-data %s" % (len(non_data), non_data)))
+        # Call the superclass constructor
+        super(ParadJCsppParser, self).__init__(config,
+                                                 state,
+                                                 stream_handle,
+                                                 state_callback,
+                                                 publish_callback,
+                                                 exception_callback,
+                                                 DATA_REGEX,
+                                                 ignore_matcher=IGNORE_MATCHER,
+                                                 *args, **kwargs)
