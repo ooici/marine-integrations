029aed5d34ba5bcc4798e575d39dea0636161ff7
Joseph J Padula <Joseph_J_Padula@raytheon.com>
2014-07-25 09:59:56 -0700

    parad_j_cspp qual test completed.

--- mi/dataset/driver/parad_j/cspp/test/test_driver.py
+++ mi/dataset/driver/parad_j/cspp/test/test_driver.py
@@ -20,10 +20,14 @@ import os
 
 from nose.plugins.attrib import attr
 
+from pyon.agent.agent import ResourceAgentState
+from interface.objects import ResourceAgentErrorEvent
+from interface.objects import ResourceAgentConnectionLostErrorEvent
+
+
 from mi.core.log import get_logger
 log = get_logger()
-
-from mi.idk.exceptions import SampleTimeout
+from mi.core.instrument.instrument_driver import DriverEvent
 
 from mi.idk.dataset.unit_test import DataSetTestCase
 from mi.idk.dataset.unit_test import DataSetIntegrationTestCase
@@ -31,7 +35,8 @@ from mi.idk.dataset.unit_test import DataSetQualificationTestCase
 
 from mi.dataset.dataset_driver import \
     DataSourceConfigKey, \
-    DataSetDriverConfigKeys
+    DataSetDriverConfigKeys, \
+    DriverParameter
 
 from mi.dataset.driver.parad_j.cspp.driver import \
     ParadJCsppDataSetDriver, \
@@ -42,7 +47,8 @@ from mi.dataset.parser.parad_j_cspp import \
     ParadJCsppMetadataTelemeteredDataParticle, \
     ParadJCsppMetadataRecoveredDataParticle, \
     ParadJCsppInstrumentTelemeteredDataParticle, \
-    ParadJCsppInstrumentRecoveredDataParticle
+    ParadJCsppInstrumentRecoveredDataParticle, \
+    DataParticleType
 
 DIR_PARAD_TELEMETERED = '/tmp/parad/telem/test'
 DIR_PARAD_RECOVERED = '/tmp/parad/recov/test'
@@ -298,32 +304,228 @@ class QualificationTest(DataSetQualificationTestCase):
         Setup an agent/driver/harvester/parser and verify that data is
         published out the agent
         """
-        pass
+        log.info("=========== START QUAL TEST PUBLISH PATH =================")
+
+        self.create_sample_data_set_dir('11079364_PPD_PARS.txt', DIR_PARAD_TELEMETERED)
+        self.create_sample_data_set_dir('11079364_PPB_PARS.txt', DIR_PARAD_RECOVERED)
+
+        self.assert_initialize()
+
+        # get the telemetered metadata particle
+        result1 = self.data_subscribers.get_samples(DataParticleType.METADATA_TELEMETERED, 1, 10)
+        #get the telemetered instrument particles
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_TELEMETERED, 19, 40)
+        # combine the results
+        result1.extend(result2)
+
+        # check the results
+        self.assert_data_values(result1, '11079364_PPD_PARS_telem.yml')
+
+        # get the recovered metadata particle
+        result1 = self.data_subscribers.get_samples(DataParticleType.METADATA_RECOVERED, 1, 10)
+        # get the recovered instrument particle
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_RECOVERED, 19, 40)
+        # combine the results
+        result1.extend(result2)
+
+        # check the results
+        self.assert_data_values(result1, '11079364_PPB_PARS_recov.yml')
 
     def test_large_import(self):
         """
         Test importing a large number of samples from the file at once
         """
-        pass
+        log.info("=========== START QUAL TEST LARGE IMPORT =================")
+
+        # using the same file for both telemetered and recovered because
+        # there are no large telemetered files available at this time
+        # Note: the recovered (PPB) file must be renamed to be a telemetered (PPD) file when it is
+        # copied to the DIR_PARAD_TELEMETERED directory.
+        self.create_sample_data_set_dir('11079364_PPB_PARS.txt', DIR_PARAD_TELEMETERED,
+                                        '11079364_PPD_PARS.txt')
+        self.create_sample_data_set_dir('11079364_PPB_PARS.txt', DIR_PARAD_RECOVERED)
+
+        self.assert_initialize()
+
+        # # get the telemetered metadata particle
+        self.data_subscribers.get_samples(DataParticleType.METADATA_TELEMETERED, 1, 10)
+        # get ALL of the telemetered instrument particles
+        self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_TELEMETERED, 194, 120)
+
+        # get the recovered metadata particle
+        self.data_subscribers.get_samples(DataParticleType.METADATA_RECOVERED, 1, 10)
+        # get the recovered metadata particle
+        self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_RECOVERED, 194, 120)
 
     def test_stop_start(self):
         """
         Test the agents ability to start data flowing, stop, then restart
         at the correct spot.
         """
-        pass
+        self.create_sample_data_set_dir('11079364_PPD_PARS.txt', DIR_PARAD_TELEMETERED)
+        self.create_sample_data_set_dir('11079364_PPB_PARS.txt', DIR_PARAD_RECOVERED)
+
+        # Put the driver in command mode so it can be started and stopped
+        self.assert_initialize(final_state=ResourceAgentState.COMMAND)
+        self.dataset_agent_client.set_resource(
+            {DriverParameter.RECORDS_PER_SECOND: 1})
+        self.assert_start_sampling()
+
+        # get the telemetered metadata particle
+        result1 = self.data_subscribers.get_samples(DataParticleType.METADATA_TELEMETERED, 1, 10)
+        #get the first 4 telemetered instrument particles
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_TELEMETERED, 4, 40)
+        # combine the results
+        result1.extend(result2)
+
+        # check the results
+        self.assert_data_values(result1, 'test_telemetered_stop_start_one.yml')
+
+        # get the recovered metadata particle
+        result1 = self.data_subscribers.get_samples(DataParticleType.METADATA_RECOVERED, 1, 10)
+        # get the first 7 recovered instrument particle
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_RECOVERED, 7, 40)
+        # combine the results
+        result1.extend(result2)
+
+        # check the results
+        self.assert_data_values(result1, 'test_recovered_stop_start_one.yml')
+
+        # stop sampling
+        self.assert_stop_sampling()
+
+        #restart sampling
+        self.assert_start_sampling()
+
+        #get the next 12 telemetered instrument particles
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_TELEMETERED, 12, 40)
+
+        # check the results
+        self.assert_data_values(result2, 'test_telemetered_stop_start_two.yml')
+
+        # get the next 8 recovered instrument particle
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_RECOVERED, 8, 40)
+
+        # check the results
+        self.assert_data_values(result2, 'test_recovered_stop_start_two.yml')
 
     def test_shutdown_restart(self):
         """
         Test a full stop of the dataset agent, then restart the agent 
         and confirm it restarts at the correct spot.
         """
-        pass
+        self.create_sample_data_set_dir('11079364_PPD_PARS.txt', DIR_PARAD_TELEMETERED)
+        self.create_sample_data_set_dir('11079364_PPB_PARS.txt', DIR_PARAD_RECOVERED)
+
+        #put the driver in command mode so it can be started and stopped
+        self.assert_initialize(final_state=ResourceAgentState.COMMAND)
+        self.dataset_agent_client.set_resource(
+            {DriverParameter.RECORDS_PER_SECOND: 1})
+        self.assert_start_sampling()
+
+        # get the telemetered metadata particle
+        result1 = self.data_subscribers.get_samples(DataParticleType.METADATA_TELEMETERED, 1, 10)
+        #get the first 4 telemetered instrument particles
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_TELEMETERED, 4, 40)
+        # combine the results
+        result1.extend(result2)
+
+        # check the results
+        self.assert_data_values(result1, 'test_telemetered_stop_start_one.yml')
+
+        # get the recovered metadata particle
+        result1 = self.data_subscribers.get_samples(DataParticleType.METADATA_RECOVERED, 1, 10)
+        # get the first 7 recovered instrument particle
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_RECOVERED, 7, 40)
+        # combine the results
+        result1.extend(result2)
+
+        # check the results
+        self.assert_data_values(result1, 'test_recovered_stop_start_one.yml')
+
+        # stop sampling
+        self.assert_stop_sampling()
+
+        self.stop_dataset_agent_client()
+        # Re-start the agent
+        self.init_dataset_agent_client()
+        # Re-initialize
+        self.assert_initialize(final_state=ResourceAgentState.COMMAND)
+
+        #restart sampling
+        self.assert_start_sampling()
+
+        #get the next 12 telemetered instrument particles
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_TELEMETERED, 12, 40)
+
+        # check the results
+        self.assert_data_values(result2, 'test_telemetered_stop_start_two.yml')
+
+        # get the next 8 recovered instrument particle
+        result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT_RECOVERED, 8, 40)
+
+        # check the results
+        self.assert_data_values(result2, 'test_recovered_stop_start_two.yml')
 
     def test_parser_exception(self):
         """
         Test an exception is raised after the driver is started during
         record parsing.
         """
-        pass
+        log.info("=========== START QUAL TEST PARSER EXCEPTION =================")
+
+        self.create_sample_data_set_dir('11079364_BAD_PPB_PARS.txt', DIR_PARAD_RECOVERED)
+
+        self.assert_initialize()
+
+        self.assert_event_received(ResourceAgentErrorEvent, 10)
+
+    def test_harvester_new_file_exception(self):
+        """
+        Test an exception raised after the driver is started during
+        the file read.
+
+        exception callback called.
+        """
+        # need to put data in the file, not just make an empty file for this to work
+        self.create_sample_data_set_dir('11079364_PPD_PARS.txt', DIR_PARAD_TELEMETERED,
+                                        mode=000)
+
+        self.assert_initialize(final_state=ResourceAgentState.COMMAND)
+
+        self.event_subscribers.clear_events()
+        self.assert_resource_command(DriverEvent.START_AUTOSAMPLE)
+        self.assert_state_change(ResourceAgentState.LOST_CONNECTION, 90)
+        self.assert_event_received(ResourceAgentConnectionLostErrorEvent, 10)
+
+        self.clear_sample_data()
+        self.create_sample_data_set_dir('11079364_PPD_PARS.txt', DIR_PARAD_TELEMETERED)
+
+        # Should automatically retry connect and transition to streaming
+        self.assert_state_change(ResourceAgentState.STREAMING, 90)
+
+    def test_harvester_new_recov_file_exception(self):
+        """
+        Test an exception raised after the driver is started during
+        the file read.
+
+        exception callback called.
+        """
+        # need to put data in the file, not just make an empty file for this to work
+        self.create_sample_data_set_dir('11079364_PPB_PARS.txt', DIR_PARAD_RECOVERED, mode=000)
+
+        self.assert_initialize(final_state=ResourceAgentState.COMMAND)
+
+        self.event_subscribers.clear_events()
+        self.assert_resource_command(DriverEvent.START_AUTOSAMPLE)
+        self.assert_state_change(ResourceAgentState.LOST_CONNECTION, 90)
+        self.assert_event_received(ResourceAgentConnectionLostErrorEvent, 10)
+
+        self.clear_sample_data()
+        self.create_sample_data_set_dir('11079364_PPB_PARS.txt', DIR_PARAD_RECOVERED)
+
+        # Should automatically retry connect and transition to streaming
+        self.assert_state_change(ResourceAgentState.STREAMING, 90)
+
+
 
