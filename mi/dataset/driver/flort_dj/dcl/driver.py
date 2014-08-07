a9a326963f7a2721fbb64bbb125e0fe1875e168e
Steven Myerson <Steven_G_Myerson@raytheon.com>
2014-08-07 06:48:47 -0700

    Interim update

---
+++ mi/dataset/driver/flort_dj/dcl/driver.py
@@ -0,0 +1,53 @@
+"""
+@package mi.dataset.driver.flort_dj.dcl.driver
+@file marine-integrations/mi/dataset/driver/flort_dj/dcl/driver.py
+@author Steve Myerson
+@brief Driver for the flort_dj_dcl
+Release notes:
+
+Initial Release
+"""
+
+__author__ = 'Steve Myerson'
+__license__ = 'Apache 2.0'
+
+import string
+
+from mi.core.log import get_logger ; log = get_logger()
+
+from mi.dataset.dataset_driver import SimpleDataSetDriver
+from mi.dataset.parser.flort_dj_dcl import FlortDjDclParser, FlortDjDclParserDataParticle
+
+class FlortDjDclDataSetDriver(SimpleDataSetDriver):
+    
+    @classmethod
+    def stream_config(cls):
+        return [FlortDjDclParserDataParticle.type()]
+
+    def _build_parser(self, parser_state, infile):
+        """
+        Build and return the parser
+        """
+        config = self._parser_config
+        config.update({
+            'particle_module': 'mi.dataset.parser.flort_dj_dcl',
+            'particle_class': 'FlortDjDclParserDataParticle'
+        })
+        log.debug("My Config: %s", config)
+        self._parser = FlortDjDclParser(
+            config,
+            parser_state,
+            infile,
+            self._save_parser_state,
+            self._data_callback,
+            self._sample_exception_callback 
+        )
+        return self._parser
+
+    def _build_harvester(self, driver_state):
+        """
+        Build and return the harvester
+        """
+        # *** Replace the following with harvester initialization ***
+        self._harvester = None     
+        return self._harvester
