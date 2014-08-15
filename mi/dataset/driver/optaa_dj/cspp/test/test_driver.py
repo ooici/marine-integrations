6594580e1be2382c356afac09aa72f6029f48313
Joseph J Padula <Joseph_J_Padula@raytheon.com>
2014-08-15 08:05:50 -0700

    optaa_dj_cspp integration tests completed.

--- mi/dataset/driver/optaa_dj/cspp/test/test_driver.py
+++ mi/dataset/driver/optaa_dj/cspp/test/test_driver.py
@@ -15,42 +15,80 @@ USAGE:
 __author__ = 'Joe Padula'
 __license__ = 'Apache 2.0'
 
-import unittest
+import hashlib
+import os
 
 from nose.plugins.attrib import attr
-from mock import Mock
 
-from mi.core.log import get_logger ; log = get_logger()
-from mi.idk.exceptions import SampleTimeout
+from pyon.agent.agent import ResourceAgentState
+from interface.objects import ResourceAgentErrorEvent
+
+from mi.core.log import get_logger
+log = get_logger()
 
 from mi.idk.dataset.unit_test import DataSetTestCase
 from mi.idk.dataset.unit_test import DataSetIntegrationTestCase
 from mi.idk.dataset.unit_test import DataSetQualificationTestCase
 
-from mi.dataset.dataset_driver import DataSourceConfigKey, DataSetDriverConfigKeys
-from mi.dataset.driver.optaa_dj.cspp.driver import OptaaDjCsppDataSetDriver
-from mi.dataset.parser.optaa_dj_cspp import OptaaDjCsppParserDataParticle
+from mi.dataset.dataset_driver import \
+    DataSourceConfigKey, \
+    DataSetDriverConfigKeys, \
+    DriverParameter
 
-# Fill in driver details
+from mi.dataset.driver.optaa_dj.cspp.driver import \
+    OptaaDjCsppDataSetDriver, \
+    DataTypeKey
+
+from mi.dataset.parser.cspp_base import StateKey
+from mi.dataset.parser.optaa_dj_cspp import \
+    OptaaDjCsppMetadataTelemeteredDataParticle, \
+    OptaaDjCsppMetadataRecoveredDataParticle, \
+    OptaaDjCsppInstrumentTelemeteredDataParticle, \
+    OptaaDjCsppInstrumentRecoveredDataParticle, \
+    DataParticleType
+
+DIR_OPTAA_TELEMETERED = '/tmp/optaa/telem/test'
+DIR_OPTAA_RECOVERED = '/tmp/optaa/recov/test'
+
+OPTAA_REC_PATTERN = '*ACS_ACS.txt'
+OPTAA_TEL_PATTERN = '*ACD_ACS.txt'
+
+RECOVERED_SAMPLE_DATA = '11079364_ACS_ACS.txt'
+TELEMETERED_SAMPLE_DATA = '11079364_ACD_ACS.txt'
+
+# Driver details
 DataSetTestCase.initialize(
     driver_module='mi.dataset.driver.optaa_dj.cspp.driver',
     driver_class='OptaaDjCsppDataSetDriver',
-    agent_resource_id = '123xyz',
-    agent_name = 'Agent007',
-    agent_packet_config = OptaaDjCsppDataSetDriver.stream_config(),
-    startup_config = {
+    agent_resource_id='123xyz',
+    agent_name='Agent007',
+    agent_packet_config=OptaaDjCsppDataSetDriver.stream_config(),
+    startup_config={
         DataSourceConfigKey.RESOURCE_ID: 'optaa_dj_cspp',
         DataSourceConfigKey.HARVESTER:
         {
-            DataSetDriverConfigKeys.DIRECTORY: '/tmp/dsatest',
-            DataSetDriverConfigKeys.PATTERN: '',
-            DataSetDriverConfigKeys.FREQUENCY: 1,
+            DataTypeKey.OPTAA_DJ_CSPP_TELEMETERED: {
+                DataSetDriverConfigKeys.DIRECTORY: DIR_OPTAA_TELEMETERED,
+                DataSetDriverConfigKeys.PATTERN: OPTAA_TEL_PATTERN,
+                DataSetDriverConfigKeys.FREQUENCY: 1,
+            },
+            DataTypeKey.OPTAA_DJ_CSPP_RECOVERED: {
+                DataSetDriverConfigKeys.DIRECTORY: DIR_OPTAA_RECOVERED,
+                DataSetDriverConfigKeys.PATTERN: OPTAA_REC_PATTERN,
+                DataSetDriverConfigKeys.FREQUENCY: 1,
+            }
         },
-        DataSourceConfigKey.PARSER: {}
+        DataSourceConfigKey.PARSER: {
+            DataTypeKey.OPTAA_DJ_CSPP_TELEMETERED: {},
+            DataTypeKey.OPTAA_DJ_CSPP_RECOVERED: {}
+        }
     }
 )
 
-SAMPLE_STREAM = 'optaa_dj_cspp_parsed'
+REC_PARTICLES = (OptaaDjCsppMetadataRecoveredDataParticle,
+                 OptaaDjCsppInstrumentRecoveredDataParticle)
+TEL_PARTICLES = (OptaaDjCsppMetadataTelemeteredDataParticle,
+                 OptaaDjCsppInstrumentTelemeteredDataParticle)
 
 # The integration and qualification tests generated here are suggested tests,
 # but may not be enough to fully test your driver. Additional tests should be
@@ -64,32 +102,148 @@ SAMPLE_STREAM = 'optaa_dj_cspp_parsed'
 @attr('INT', group='mi')
 class IntegrationTest(DataSetIntegrationTestCase):
  
+    def get_file_state(self, path, ingested=False, position=None, metadata_extracted=False):
+        """
+        Create a state object for a file. If a position is passed then add a parser state as well.
+        """
+        mod_time = os.path.getmtime(path)
+        file_size = os.path.getsize(path)
+        with open(path) as filehandle:
+            md5_checksum = hashlib.md5(filehandle.read()).hexdigest()
+
+        parser_state = {
+            StateKey.POSITION: position,
+            StateKey.METADATA_EXTRACTED: metadata_extracted
+        }
+
+        return {
+            'ingested': ingested,
+            'file_mod_date': mod_time,
+            'file_checksum': md5_checksum,
+            'file_size': file_size,
+            'parser_state': parser_state
+        }
+
     def test_get(self):
         """
-        Test that we can get data from files.  Verify that the driver
-        sampling can be started and stopped
+        Test that we can get data from files.
+        Assert that the particles are correct.
         """
-        pass
+        log.info("================ START INTEG TEST GET =====================")
 
-    def test_stop_resume(self):
-        """
-        Test the ability to stop and restart the process
-        """
-        pass
+        # Start sampling.
+        self.driver.start_sampling()
+        self.clear_async_data()
 
-    def test_stop_start_resume(self):
+        # test that everything works for the telemetered harvester
+        self.create_sample_data_set_dir(TELEMETERED_SAMPLE_DATA, DIR_OPTAA_TELEMETERED)
+
+        log.debug('### Sample file created in dir = %s ', DIR_OPTAA_TELEMETERED)
+
+        # check the metadata particle and the first 19 instrument particles
+        self.assert_data(TEL_PARTICLES,
+                         '11079364_ACD_ACS_telem.yml',
+                         count=20, timeout=60)
+
+        # test that everything works for the recovered harvester
+        self.create_sample_data_set_dir(RECOVERED_SAMPLE_DATA, DIR_OPTAA_RECOVERED)
+
+        log.debug('### Sample file created in dir = %s ', DIR_OPTAA_RECOVERED)
+
+        # check the metadata particle and the first 19 instrument particles
+        self.assert_data(REC_PARTICLES,
+                         '11079364_ACS_ACS_recov.yml',
+                         count=20, timeout=60)
+
+    def test_mid_state_start(self):
+        """
+        Test the ability to start the driver with a saved state
+        """
+        log.info("================ START INTEG TEST MID STATE START =====================")
+
+        recovered_file_one = RECOVERED_SAMPLE_DATA
+        telemetered_file_one = TELEMETERED_SAMPLE_DATA
+
+        recovered_path_1 = self.create_sample_data_set_dir(recovered_file_one, DIR_OPTAA_RECOVERED)
+        telemetered_path_1 = self.create_sample_data_set_dir(telemetered_file_one, DIR_OPTAA_TELEMETERED)
+
+        # For recovered, we will assume first 19 records have been processed and put position to 20
+        # For telemetered, we will assume first 13 records have been processed and put position to 14
+        state = {
+            DataTypeKey.OPTAA_DJ_CSPP_RECOVERED: {
+                recovered_file_one: self.get_file_state(recovered_path_1,
+                                                        ingested=False,
+                                                        position=35725,
+                                                        metadata_extracted=True),
+            },
+            DataTypeKey.OPTAA_DJ_CSPP_TELEMETERED: {
+                telemetered_file_one: self.get_file_state(telemetered_path_1,
+                                                          ingested=False,
+                                                          position=24168,
+                                                          metadata_extracted=True),
+            }
+        }
+
+        driver = self._get_driver_object(memento=state)
+
+        # create some data to parse
+        self.clear_async_data()
+
+        driver.start_sampling()
+
+        # verify data is produced
+        self.assert_data(REC_PARTICLES, 'test_recovered_midstate_start.yml', count=1, timeout=60)
+
+        self.assert_data(TEL_PARTICLES, 'test_telemetered_midstate_start.yml', count=2, timeout=60)
+
+    def test_start_stop_resume(self):
         """
         Test the ability to stop and restart sampling, ingesting files in the
         correct order
         """
-        pass
+        recovered_file_one = RECOVERED_SAMPLE_DATA
+        telemetered_file_one = TELEMETERED_SAMPLE_DATA
+
+        self.create_sample_data_set_dir(recovered_file_one, DIR_OPTAA_RECOVERED)
+        self.create_sample_data_set_dir(telemetered_file_one, DIR_OPTAA_TELEMETERED)
+
+        # create some data to parse
+        self.clear_async_data()
+
+        self.driver.start_sampling()
+
+        # verify data is produced
+        self.assert_data(REC_PARTICLES, 'test_recovered_start_stop_resume_one.yml', count=1, timeout=10)
+
+        self.assert_data(TEL_PARTICLES, 'test_telemetered_start_stop_resume_one.yml', count=1, timeout=10)
+
+        self.driver.stop_sampling()
+
+        self.driver.start_sampling()
+
+        # verify data is produced
+        self.assert_data(REC_PARTICLES, 'test_recovered_start_stop_resume_two.yml', count=4, timeout=60)
+
+        self.assert_data(TEL_PARTICLES, 'test_telemetered_start_stop_resume_two.yml', count=4, timeout=10)
+
+        self.driver.stop_sampling()
 
     def test_sample_exception(self):
         """
         Test a case that should produce a sample exception and confirm the
         sample exception occurs
         """
-        pass
+        log.info("================ START INTEG TEST SAMPLE EXCEPTION =====================")
+
+        # Start sampling.
+        self.driver.start_sampling()
+        self.clear_async_data()
+
+        # test that everything works for the telemetered harvester
+        self.create_sample_data_set_dir('11079364_BAD_ACS_ACS.txt', DIR_OPTAA_RECOVERED)
+
+        # an event catches the sample exception
+        self.assert_event('ResourceAgentErrorEvent')
 
 ###############################################################################
 #                            QUALIFICATION TESTS                              #
