d5a8a5fd4beb6ff5127abe049b98300115d6c140
Joseph J Padula <Joseph_J_Padula@raytheon.com>
2014-08-22 11:55:49 -0700

    Added generated files from start_driver for flord_l_wfp.

---
+++ mi/dataset/driver/flord_l/wfp/driver.py
@@ -0,0 +1,53 @@
+"""
+@package mi.dataset.driver.flord_l.wfp.driver
+@file marine-integrations/mi/dataset/driver/flord_l/wfp/driver.py
+@author Joe Padula
+@brief Driver for the flord_l_wfp
+Release notes:
+
+Initial Release
+"""
+
+__author__ = 'Joe Padula'
+__license__ = 'Apache 2.0'
+
+import string
+
+from mi.core.log import get_logger ; log = get_logger()
+
+from mi.dataset.dataset_driver import SimpleDataSetDriver
+from mi.dataset.parser.flord_l_wfp import FlordLWfpParser, FlordLWfpParserDataParticle
+
+class FlordLWfpDataSetDriver(SimpleDataSetDriver):
+    
+    @classmethod
+    def stream_config(cls):
+        return [FlordLWfpParserDataParticle.type()]
+
+    def _build_parser(self, parser_state, infile):
+        """
+        Build and return the parser
+        """
+        config = self._parser_config
+        config.update({
+            'particle_module': 'mi.dataset.parser.flord_l_wfp',
+            'particle_class': 'FlordLWfpParserDataParticle'
+        })
+        log.debug("My Config: %s", config)
+        self._parser = FlordLWfpParser(
+            config,
+            parser_state,
+            infile,
+            self._save_parser_state,
+            self._data_callback,
+            self._sample_exception_callback 
+        )
+        return self._parser
+
+    def _build_harvester(self, driver_state):
+        """
+        Build and return the harvester
+        """
+        # *** Replace the following with harvester initialization ***
+        self._harvester = None     
+        return self._harvester
